5d6807f8def11800fac16af5557fbc8d652a25fa
Kim Ng <kimleeng@gmail.com>
2025-06-12 09:38:41 +0200

    post dagster init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ubio_autobox",
+    packages=find_packages(exclude=["ubio_autobox_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
